cb1f04717588abf7cfc4876fe23a5c91c59fecb1
DonlerSun <13238041502@163.com>
2016-07-28 17:38:04 +0800

    new

---
+++ MyDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyDemo
+//
+//  Created by Apple on 16/7/28.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor orangeColor];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
